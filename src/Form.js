d3e13bfc16922e34040f1192e53e5ffe33f3ed6d
Shiva Singh <shivasingh.200087@gmail.com>
2022-04-08 08:53:59 +0530

    done

--- src/Form.js
+++ src/Form.js
@@ -1,7 +1,8 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { auth, getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 export default function Form() {
+    const [details, setDetails] = useState('');
 
     const x = useRef(null);
     const y = useRef(null);
@@ -29,7 +30,6 @@ export default function Form() {
         auth.onAuthStateChanged(function (user) {
             if (user) {
                 document.getElementById('quickstart-sign-in').textContent = 'Sign out';
-                // window.location.href=hostname+"main.php";
             } else {
                 document.getElementById('quickstart-sign-in').textContent = 'Sign in with Google';
             }
@@ -37,6 +37,7 @@ export default function Form() {
         });
 
         document.getElementById('quickstart-sign-in').addEventListener('click', handleWithGoogle, false);
+        document.getElementById('quickstart-sign-out').addEventListener('click', handleWithGoogle, false);
     }
 
     window.onload = function () {
@@ -47,82 +48,103 @@ export default function Form() {
     const provider = new GoogleAuthProvider();
 
     const handleWithGoogle = () => {
-        signInWithPopup(auth, provider)
-            .then((result) => {
-                // This gives you a Google Access Token. You can use it to access the Google API.
-                const credential = GoogleAuthProvider.credentialFromResult(result);
-                const token = credential.accessToken;
-                // The signed-in user info.
-                const user = result.user;
-                console.log(user);
-                // ...
-            }).catch((error) => {
-                // Handle Errors here.
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                // The email of the user's account used.
-                const email = error.email;
-                // The AuthCredential type that was used.
-                const credential = GoogleAuthProvider.credentialFromError(error);
-                // ...
-            });
-    }
 
-    if (!auth.currentUser) {
-        alert('login');
-    } else {
-        auth.signOut();
-        alert('logout')
+        if (auth.currentUser) {
+            auth.signOut();
+            setDetails('');
+        } else {
+            signInWithPopup(auth, provider)
+                .then((result) => {
+                    // This gives you a Google Access Token. You can use it to access the Google API.
+                    const credential = GoogleAuthProvider.credentialFromResult(result);
+                    const token = credential.accessToken;
+                    // The signed-in user info.
+                    setDetails(result.user);
+                    // ...
+                }).catch((error) => {
+                    // Handle Errors here.
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    // The email of the user's account used.
+                    const email = error.email;
+                    // The AuthCredential type that was used.
+                    const credential = GoogleAuthProvider.credentialFromError(error);
+                    // ...
+                });
+        }
     }
-
     return (
         <div className="contener">
             <div className="main">
-                <div className="button_box">
-                    <div id="btn" ref={z}></div>
-                    <button type="button" className="toggle_btn" onClick={logIn}>Log In</button>
-                    <button type="button" className="toggle_btn" onClick={signUp}>Sign Up</button>
-                </div>
-                {/* <!-- <p className="heading">Log In --> */}
-                <div className="google_box">
-                    <button type="button" className="btn btn-light btn_style"
-                        disabled id="quickstart-sign-in"
-                        onClick={handleWithGoogle}>Log In With GOOGLE</button>
-                </div>
-                <div id="log_in" ref={x}>
-                    <form action="signUpOk()" method="POST">
-                        <div className="form-group">
-                            <label for="exampleInputEmail1">Email address</label>
-                            <input name="email" type="email" className="form-control" id="exampleInputEmail1" placeholder="Enter email" />
+                {details === '' &&
+                    <>
+                        <div className="button_box">
+                            <div id="btn" ref={z}></div>
+                            <button type="button" className="toggle_btn" onClick={logIn}>Log In</button>
+                            <button type="button" className="toggle_btn" onClick={signUp}>Sign Up</button>
                         </div>
-                        <div className="form-group">
-                            <label for="exampleInputPassword1">Password</label>
-                            <input name="password" type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                        {/* <!-- <p className="heading">Log In --> */}
+                        <div className="google_box">
+                            <button type="button" className="btn btn-light btn_style"
+                                disabled id="quickstart-sign-in"
+                                onClick={handleWithGoogle}>Log In With GOOGLE</button>
                         </div>
-                        <div className="form-group form-check">
-                            <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                            <label className="form-check-label" for="exampleCheck1">Remember me</label>
+                        <div id="log_in" ref={x}>
+                            <form action="signUpOk()" method="POST">
+                                <div className="form-group">
+                                    <label for="exampleInputEmail1">Email address</label>
+                                    <input name="email" type="email" className="form-control" id="exampleInputEmail1" placeholder="Enter email" />
+                                </div>
+                                <div className="form-group">
+                                    <label for="exampleInputPassword1">Password</label>
+                                    <input name="password" type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                                </div>
+                                <div className="form-group form-check">
+                                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                                    <label className="form-check-label" for="exampleCheck1">Remember me</label>
+                                </div>
+                                <button type="button" className="btn btn-light btn_style">Log In</button>
+                            </form>
                         </div>
-                        <button type="button" className="btn btn-light btn_style">Log In</button>
-                    </form>
-                </div>
-                <div id="sign_up" ref={y}>
-                    <form action="signUpOk()" method="POST">
-                        <div className="form-group">
-                            <label for="exampleInputEmail1">Email address</label>
-                            <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Enter email" />
+                        <div id="sign_up" ref={y}>
+                            <form action="signUpOk()" method="POST">
+                                <div className="form-group">
+                                    <label for="exampleInputEmail1">Email address</label>
+                                    <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Enter email" />
+                                </div>
+                                <div className="form-group">
+                                    <label for="exampleInputPassword1">Password</label>
+                                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                                </div>
+                                <div className="form-group form-check">
+                                    <input type="checkbox" className="form-check-input" id="exampleCheck2" />
+                                    <label className="form-check-label" for="exampleCheck2">I agree to Term & Condition</label>
+                                </div>
+                                <button type="button" className="btn btn-light btn_style">Sign Up</button>
+                            </form>
                         </div>
-                        <div className="form-group">
-                            <label for="exampleInputPassword1">Password</label>
-                            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                    </>}
+
+                {details != '' &&
+                    <div  className='text-center' style={{marginLeft:'-10px'}}>
+                        <div className="google_box">
+                            <button type="button" className="btn btn-light btn_style" id="quickstart-sign-out"
+                                onClick={handleWithGoogle}>Sign Out</button>
                         </div>
-                        <div className="form-group form-check">
-                            <input type="checkbox" className="form-check-input" id="exampleCheck2" />
-                            <label className="form-check-label" for="exampleCheck2">I agree to Term & Condition</label>
+                        <div className='p-3'>
+                            <img src={details.photoURL} />
                         </div>
-                        <button type="button" className="btn btn-light btn_style">Sign Up</button>
-                    </form>
-                </div>
+                        <div className='h6'>
+                            {details.uid}
+                        </div>
+                        <div className='h2'>
+                            {details.displayName}
+                        </div>
+                        <div className='h5'>
+                            {details.email}<br/>
+                            {details.phoneNumber}
+                        </div>
+                    </div>}
             </div>
         </div>)
 }
